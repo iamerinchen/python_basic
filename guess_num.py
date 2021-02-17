8dc2b97934ed0e2ee23739c666d6a916e04dea03
Erin Chen <iamerin.c@gmail.com>
2021-02-17 16:48:16 +0800

    add guess_num.py

---
+++ guess_num.py
@@ -0,0 +1,13 @@
+import random
+r = random.randint(1,100)
+
+while True:
+    num = input('Please enter a num[1-100]: ')
+    num = int(num)
+    if r == num:
+        print('Bingo!!')
+        break
+    elif num > r:
+        print('the answer is smaller than '+ str(num))
+    elif num < r:
+        print('the answer is bigger than ' + str(num))
