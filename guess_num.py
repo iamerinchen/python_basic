8ed66b70fb21d0a9929cb9ef62b12d70e3cd6186
Erin Chen <iamerin.c@gmail.com>
2021-02-18 10:21:46 +0800

    add count for guess_num.py

--- guess_num.py
+++ guess_num.py
@@ -1,13 +1,15 @@
 import random
 r = random.randint(1,100)
-
+count = 0
 while True:
+    count = count + 1
     num = input('Please enter a num[1-100]: ')
     num = int(num)
     if r == num:
-        print('Bingo!! the answer is ' + str(r))
+        print('Bingo!! the answer is ' , r)
         break
     elif num > r:
-        print('the answer is smaller than ' + str(num))
+        print('the answer is smaller than ', num)
     elif num < r:
-        print('the answer is bigger than ' + str(num))
+        print('the answer is bigger than ', num)
+    print('You have guessed' , count , 'times!')
